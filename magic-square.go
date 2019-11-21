0598129d0cda2e690024593f3244fd2339b3a718
John McKee <jmckee3@mac.com>
2019-11-20 17:04:03 -0800

    initial commit

---
+++ magic-square.go
@@ -0,0 +1,10 @@
+package main
+
+func formingMagicSquare(s [][]int32) int32 {
+
+	return 0
+}
+
+func main() {
+
+}
---
+++ spec-magic-square_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMagicSquareSample1(t *testing.T) {
+	// scores := []int32{100, 100, 50, 40, 40, 20, 10}
+	// alice := []int32{5, 25, 50, 120}
+
+	// rank := climbingLeaderboard(scores, alice)
+	// fmt.Println(rank)
+	// if rank[0] != 6 ||
+	// 	rank[1] != 4 ||
+	// 	rank[2] != 2 ||
+	// 	rank[3] != 1 {
+	// 	t.Errorf("got %+v instead of [6, 4, 2, 1]", rank)
+	// }
+}
